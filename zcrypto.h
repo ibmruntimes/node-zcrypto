c30bcfafdcd245e7dbeddc0a99b693829aec3d20
Igor <igor.todorovski@gmail.com>
2020-05-06 22:45:40 -0400

    Update copyrights

--- zcrypto.h
+++ zcrypto.h
@@ -1,3 +1,9 @@
+/*
+ * Licensed Materials - Property of IBM
+ * (C) Copyright IBM Corp. 2020. All Rights Reserved.
+ * US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
 #ifndef __ZCRYPTO_H_
 #define __ZCRYPTO_H_ 1
 
